3b8f22d21730c9d010e5e72131e0f34663e5e57c
Jonathan Chen <2110511101010@live4.utcc.ac.th>
2023-03-12 14:48:56 +0700

    Bug fix#2 – adding student id validation (id must be composed of numbers between 0 and 9)

--- validate.js
+++ validate.js
@@ -1,6 +1,6 @@
 function checkSID() {
   let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
+  if (sid.length == 10 && Number.isInteger(sid)) {
     return true;
   } else {
     return false;

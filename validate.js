17e201c08fbb3947cdd11a4377a88b5335160059
Rachata <2110511101041@live4.utcc.ac.th>
2023-03-12 14:21:22 +0700

    Bug fix#2–adding student id validation(id must becomposed of numbersbetween 0 and 9)

--- validate.js
+++ validate.js
@@ -1,12 +1,10 @@
 function checkSID() {
   let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
-    return true;
-  } else {
+  if ((sid.length == 10) && !isNaN(sid)) {
+	return true;
+} else {
     return false;
-  }
-}
-
+	}}
 function checkCandiNo() {
   let candi = (document.getElementById("candi").value).trim();
   if (isNaN(candi) || candi < 1) {

439ec1615df8c88909f4f76f971f96e12b322efe
Joseph <2110511101009@live4.utcc.ac.th>
2023-03-12 14:59:34 +0700

    Bug fix#2 -adding student id validation (id must be composed of number between 0 and 9

--- validate.js
+++ validate.js
@@ -1,10 +1,20 @@
 function checkSID() {
   let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
-    return true;
-  } else {
-    return false;
+
+  if (isNaN(sid))
+  {
+	return false;
   }
+  
+	if (sid.length == 10 ) 
+	{
+		return true;
+	} 
+	  else 
+	  {
+		return false;
+	  }
+
 }
 
 function checkCandiNo() {

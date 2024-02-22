0d7f849b26387a566fa34c7833cf17f2ae68ee47
Nontapat <Antauribp@gmail.com>
2024-02-22 15:21:52 +0700

    Bug fix#3 - revise candidate no. validation

--- validate.js
+++ validate.js
@@ -11,7 +11,7 @@ function checkCandiNo() {
   let candi = (document.getElementById("candi").value).trim();
   if (isNaN(candi) || candi < 1) {
     return false;
-  } else {
+  } else if (candi >=0 && candi <=10) {
     return true;
   }
 }

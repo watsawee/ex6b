067777493f889eed4a7b3d6f05b1ca5ed58a772b
Krita <kritapat.au@gmail.com>
2024-02-22 12:01:02 +0700

    Bug fix#2 – adding student id validation (id  must be composed of numbers between 0 and 9)

--- validate.js
+++ validate.js
@@ -1,6 +1,6 @@
 function checkSID() {
   let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
+  if (sid.length == 10 && typeof sid !== 'number') {
     return true;
   } else {
     return false;

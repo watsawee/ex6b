f8280170650f47a3df63ecb40c276d3ce0bc9eeb
Panya <cittphis@gmail.com>
2024-02-22 12:00:20 +0700

    Bug fix#2 -adding student id validation(id must be composed of numbers between 0 and 9)

--- validate.js
+++ validate.js
@@ -1,6 +1,6 @@
 function checkSID() {
   let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
+  if (sid.length == 10 && sid >=0)  {
     return true;
   } else {
     return false;

67fe2522dede701f76154ded361255cdd29a7f34
Chakkaphob <2210511101015@live4.utcc.ac.th>
2024-02-22 15:00:26 +0700

    Revert "Bug fix#2 - Adding student id validataion(id must be composed of numbers between 0 and 9)"
    This reverts commit bf3883f55c7b0f749a2bb41f1af4267a30583e17.

--- validate.js
+++ validate.js
@@ -1,6 +1,6 @@
 function checkSID() {
   let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10 && sid >= 0) {
+  if (sid.length == 10) {
     return true;
   } else {
     return false;

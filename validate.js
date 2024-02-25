efad841354f61cda186b0b29c3cdc292186aa6fb
Liondetnarin <detnarinchaiaksorn3@gmail.com>
2024-02-25 14:26:37 +0700

    Bug fix#2 - adding student id validation (id must be composed of number between 0 and 9)

--- validate.js
+++ validate.js
@@ -1,6 +1,6 @@
 function checkSID() {
   let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
+  if (sid.length == 9) {
     return true;
   } else {
     return false;
